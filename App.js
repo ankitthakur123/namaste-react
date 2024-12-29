252ff247573cb7dddf294ee6df0e858e551b32e4
ankitthakur123 <ankit2001thakur123@gmail.com>
2024-12-29 16:53:03 +0530

    namaste react epi

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-/*const heading = React.createElement("h1", {id: "heading"}, "hello world using react")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(heading) //return a javascript object
-root.render(heading)*/
-
-// creating nested element in react
+import React from "react"
+import ReactDOM from "react-dom/client"
 const parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"}, 
-        [React.createElement("h1", {id: "headingtag"}, "h1 tag"), 
-        React.createElement("h2", {id: "headingtag2"}, "h2 tag")] // we can create more children using array
+    React.createElement("div", {id: "child"},  
+        [React.createElement("h1", {id: "headingtag"}, "This is Namaste React"), 
+        React.createElement("h2", {id: "headingtag2"}, "by Ankit Thakur")]
     ))
-
+//console.log(parent)
+const jsxheading = <h1 id="jsxheading">Hii i am Ankit Thakur</h1> 
+//console.log(jsxheading)
+// react functional component
+const Title = ()=>(
+    <h1 id="heading">Hii i am Ankit Thakur</h1>
+)
+const Headingcomponent = ()=>{
+    return (
+        <div>
+            <Title />
+            <h1>Namaste react functional component</h1>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent) // react will only work in this root not in ur whole html file
-
-/* react is a library, its just a piece of js code 
-   and can work independtly even at any small portion of your app
-*/
+root.render(<Headingcomponent />)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Namaste React
+
+# Parcel
+- dev build
+- local server
+- HMR
+- file watching algorithm - written in c++
+- Cashing - faster builds
+- Image optimization
+- minification
+- bundling the things - as it is a bundler
+- consistent hashing
+- code splitting
+- differential bundling - support older browsers
+- Tree shaking - remove unused code
